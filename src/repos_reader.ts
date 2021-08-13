330379bc770b37b3009d2d5ea3242861bcf47cb6
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-13 18:35:24 +0900

    add required fields
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- src/repos_reader.ts
+++ src/repos_reader.ts
@@ -1,17 +1,34 @@
 class ReposReader {
   private repos_path_: string;
-  private update_package_name_: string;
+  private target_repos_path_: string;
+  private target_package_name_: string;
+  private target_version_: string;
 
   get repos_path(): string {
     return this.repos_path_;
   }
 
-  get update_package_name(): string {
-    return this.update_package_name_;
+  get target_package_name(): string {
+    return this.target_package_name_;
   }
 
-  constructor(repos_path: string, update_package_name: string) {
+  get target_repos_path(): string {
+    return this.target_repos_path_;
+  }
+
+  get target_version(): string {
+    return this.target_version_;
+  }
+
+  constructor(
+    repos_path: string,
+    target_repos_path: string,
+    target_package_name: string,
+    target_version: string
+  ) {
     this.repos_path_ = repos_path;
-    this.update_package_name_ = update_package_name;
+    this.target_repos_path_ = target_repos_path;
+    this.target_package_name_ = target_package_name;
+    this.target_version_ = target_version;
   }
 }
