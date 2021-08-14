1abc76315691accd5ded8467dd2ca8cfd243f87a
Masaya Kataoka <ms.kataoka@gmail.com>
2021-08-14 14:11:51 +0900

    add entry point
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- src/update_repos_action.ts
+++ src/update_repos_action.ts
@@ -1,4 +1,12 @@
 import { ReposUpdater } from "./repos_updater";
+// const core = require('@actions/core');
+import { getInput, setFailed } from "@actions/core";
 
-function main() {
+async function main() {
+  try {
+  } catch (error) {
+    setFailed(error.message);
+  }
 }
+
+main();
